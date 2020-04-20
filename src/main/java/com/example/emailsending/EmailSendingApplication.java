694ef5c332bee78af63a44c9758c4aace90d3b8a
valyippee <valerieyipfh@gmail.com>
2020-04-20 15:52:50 +0800

    initial commit

---
+++ src/main/java/com/example/emailsending/EmailSendingApplication.java
@@ -0,0 +1,13 @@
+package com.example.emailsending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSendingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSendingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/emailsending/EmailSendingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emailsending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSendingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
