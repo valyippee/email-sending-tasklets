1b85ae97cb2b30e4f5118cf5b7c7129e0aade80b
valyippee <valerieyipfh@gmail.com>
2020-05-06 21:49:09 +0800

    code cleanup

--- src/main/java/com/example/emailsending/EmailSendingApplication.java
+++ src/main/java/com/example/emailsending/EmailSendingApplication.java
@@ -5,33 +5,35 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAutoConfiguration
 public class EmailSendingApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(EmailSendingApplication.class);
 
 	@Autowired
-	JobLauncher jobLauncher;
+	private JobLauncher jobLauncher;
 
 	@Autowired
-	Job job;
+	private Job job;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailSendingApplication.class, args);
-
 	}
 
 	/**
 	 * job is launched every day from tuesday to saturday
-	 * @throws Exception
 	 */
-	@Scheduled(cron = "0 0 0 ? * TUE,WED,THU,FRI,SAT *")
+
+	@Scheduled(cron = "${cronExpression}")
 	public void launchJob() {
 		try {
 			JobParameters params = new JobParametersBuilder()
